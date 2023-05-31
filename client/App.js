9aef4d3887887fc8a5d631ae006de9241ad5c242
exxsid <cortezleoanthony5@gmail.com>
2023-05-31 18:37:12 +0800

    added code for supressing warning

--- client/App.js
+++ client/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { NativeBaseProvider, Box, Text, Center } from "native-base";
 import "react-native-gesture-handler";
 
+console.disableYellowBox = true;
+
 import { BottomNavigator } from "./config/navigation";
 
 export default function App() {
